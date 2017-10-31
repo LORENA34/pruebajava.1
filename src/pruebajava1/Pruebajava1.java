8fb70752dc2afe105a878512d898282d151225fa
lorena salinas <nena_sr83@hotmail.com>
2017-10-30 20:51:18 -0400

    Creacion de clase

---
+++ src/pruebajava1/Pruebajava1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+import java.util.Scanner;
+/*******************************************************************************
+ *Lorena Salinas Rojo
+ * @author LAB15
+ * Fecha: Octubre 30 de 2017
+ * Programa:prueba
+ *Responsabilidad: Area
+*/
+
+ public class Pruebajava1 
+
+ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   int numerobase;//Declaracion de variable tipo entero base
+   int numeroaltura;//Declaracion variable tipo entera altura
+   Scanner escaner = new Scanner (System.in);
+   System.out.println("ingrese el numero de la base de su triangulo");
+   numerobase=escaner.nextlnt();
+   System.out.println("ingrese el numero de altura de su triangulo");
+   numeroaltura=escaner.nextlnt();
+   System.out.println("El area de un rectangulo de base "+base+ y altura "+altura+")es igual a "+areaRectangulo)";
+   +(numerobase*numeroaltura));
+    }
+    
+}
